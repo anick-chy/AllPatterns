fdad5a2adbe52623965a6cb9dfffb00d226c96c2
anick-chy <anickchowdhury@gmail.com>
2015-10-15 19:18:23 +0600

    some changes of factory
    factory changes

---
+++ FactoryPattern/PizzaStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public abstract class PizzaStore
+    {
+        //SimplePizzaFactory factory;
+
+        //public PizzaStore(SimplePizzaFactory factory)
+        //{
+        //    this.factory = factory;
+        //}
+
+        public Pizza OrderPizza(string type)
+        {
+            Pizza pizza;
+            //factory will decide which pizza will be created
+            //pizza = factory.createPizza(type);
+            pizza = createPizza(type);
+            return pizza;
+        }
+
+        abstract Pizza createPizza(string type);
+    }
+}
---
+++ FactoryPattern/SimplePizzaFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class SimplePizzaFactory
+    {
+        public Pizza createPizza(string type)
+        {
+            Pizza pizza = null;
+            if (type.Equals("pizza"))
+                pizza = new Pizza();
+            return pizza;
+        }
+    }
+}
